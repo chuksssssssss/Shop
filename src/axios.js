8d154474176b7397948f3b84bfebaff07278ad4b
chuksssssssss <manesargsyannn@gmail.com>
2023-11-10 16:58:49 +0400

    Step_one

---
+++ src/axios.js
@@ -0,0 +1,12 @@
+import Axios from 'axios';
+
+const axios = Axios.create({
+	baseURL: "http://localhost:8000/api",
+	withCredentials: true,
+	headers: {
+		"Content-Type": "multipart/form-data",
+		"Accept": "application/json",
+	},
+});
+
+export default axios;
